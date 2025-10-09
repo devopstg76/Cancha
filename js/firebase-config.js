d415e7d7ccfc15a2234a13a7e1f9b9bd65fd77f2
devopstg76 <devopstg76@gmail.com>
2025-10-09 16:11:27 +0200

    addition de firebase - page Cancha reservation

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,6 +1,16 @@
-// Renommez ce fichier en firebase-config.js et remplacez par vos valeurs Firebase
-window.CANCHA_FIREBASE = { apiKey:"...", authDomain:"<proj>.firebaseapp.com", projectId:"<proj>" }
+
+// Paramètres Firebase (copie/colle depuis “Paramètres du projet → SDK Web”)
+window.CANCHA_FIREBASE = {
+  apiKey: "xxxxx",
+  authDomain: "ton-projet.firebaseapp.com",
+  projectId: "ton-projet",
+  storageBucket: "ton-projet.appspot.com",
+  messagingSenderId: "xxxxx",
+  appId: "1:xxxxx:web:xxxxx"
+};
+
+// (facultatif, mais pratique si tu veux centraliser l’URL du planning)
 window.CANCHA_SHEET = {
-  pubUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-.../pubhtml?widget=true&headers=false",
-  editUrl: "https://docs.google.com/spreadsheets/d/<ID>/edit?gid=<GID>#gid=<GID>"
-}
+  pubUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vREbb5AaLv0unzw_bwcK7Np_wbNOaKpPikh12aJDTH41ny6IKm0xGi9eqGf6OnTI8caL2iCTtsIO_Sq/pubhtml?gid=1443771080&single=true&widget=true&headers=false",
+  editUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vREbb5AaLv0unzw_bwcK7Np_wbNOaKpPikh12aJDTH41ny6IKm0xGi9eqGf6OnTI8caL2iCTtsIO_Sq/pubhtml?gid=1443771080&single=true"
+};
